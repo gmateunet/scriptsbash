86d5cae9f6949e2c7638731aca29d935affd51a7
Tu Nombre <you@example.com>
2024-05-29 13:54:49 +0200

    números aleatoris amb for

---
+++ aleatoris.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#aquest script ha de tenir un paràmetre
+#obligatori que serà la 
+#quantitat de números a genera
+if [[ ${#} -ne 1 ]]
+then
+    echo "has d'introduir un paràmetre"
+    exit 1
+fi
+
+#hem de recollir el paràmetre i guardar-lo a la variable QUANTS
+QUANTS=${1}
+
+URL="https://www.random.org/integers/?num=${QUANTS}&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+echo ${URL}
+
+#EXECUTAR URL I GUARDAM EL RESULTAT DINS LA VARIABLE NUMEROS
+# NUMEROS=$(curl ${URL})
+NUMEROS="23 52 1 20 7 8 99"
+#ARA HEM DE RECORRE AQUEST NÚMEROS PER CALCULAR LA SUMA
+SUMA=0
+echo "${NUMEROS}"
+for N in ${NUMEROS}
+do
+    # echo "NUMERO ${N}"
+    #ACUMULAR EL NÚMERO ACTUAL A LA SUMA
+    SUMA=$((SUMA+N))
+done
+
+#INFORMAR DEL RESULTAT DE LA SUMA
+echo "LA SUMA VAL: ${SUMA}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
